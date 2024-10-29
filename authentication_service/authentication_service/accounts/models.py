53dff9d24c8b98bb996d0355691744c28406eb48
Abairaj <abai.raj@scrapehero.com>
2024-10-30 01:12:22 +0530

    corrected all login and user creation

--- authentication_service/authentication_service/accounts/models.py
+++ authentication_service/authentication_service/accounts/models.py
@@ -4,49 +4,16 @@ from django.db import models
 from .managers import UserManager
 
 
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class Organization(BaseModel):
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    admins = models.ManyToManyField("User", related_name="admin", blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class JoinRequest(BaseModel):
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    emails = models.JSONField(default=list)
-    users = models.ManyToManyField(
-        "User", related_name="join_request", blank=True, null=True
-    )
-    is_admin_access = models.BooleanField(default=False)
-    is_accepted = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.user} - {self.organization}"
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True)
-    organization = models.OneToOneField(
-        Organization, on_delete=models.CASCADE, null=True
-    )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["organization"]
 
     def __str__(self):
+
         return self.email
--- authentication_service/authentication_service/accounts/urls.py
+++ authentication_service/authentication_service/accounts/urls.py
@@ -6,4 +6,16 @@ urlpatterns = [
     path("register/", views.RegisterUserView.as_view(), name="register"),
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("logout/", views.UserLogoutView.as_view(), name="logout"),
+    path(
+        "magic-link-request/",
+        views.MagicLinkRequestView.as_view(),
+        name="magic-link-request",
+    ),
+    path(
+        "magic-link-login/", views.MagicLinkLoginView.as_view(), name="magic-link-login"
+    ),
+    path(
+        "forgot-password/", views.ForgotPasswordView.as_view(), name="forgot-password"
+    ),
+    path("reset-password/", views.ResetPasswordView.as_view(), name="reset-password"),
 ]
